3dc9243de02566869c6327c082f9ab6f9d00b76f
Mert Şide <mertside@gmail.com>
2023-06-05 18:23:33 -0500

    MOV X12, XZR

--- xbMrtime_api_asm.s
+++ xbMrtime_api_asm.s
@@ -239,7 +239,7 @@ __xbrtime_put_s4_seq:
   .global __xbrtime_get_u8_seq
   .type __xbrtime_get_u8_seq, @function
 __xbrtime_get_u8_seq:
-#  MOV X12, XZR
+  MOV X12, XZR
 #.get_u8_seq:
 #  LDR X10, [X0]
 #  ADD X0, X0, X3

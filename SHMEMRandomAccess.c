0296028429ff033edc603f345502dd51a565aa55
Mert Şide <mertside@gmail.com>
2023-11-03 14:38:41 -0500

    Update SHMEMRandomAccess.c

--- SHMEMRandomAccess.c
+++ SHMEMRandomAccess.c
@@ -458,7 +458,7 @@ int main(int argc, char **argv)
 
   // if (0 == MyProc) if (outFile != stderr) fclose( outFile );
   if (0 == MyProc) fclose( outFile );
-  printf("Closed output file.\n")
+  printf("Closed output file.\n");
 
   //xbrtime_barrier();
   // for (int currentPE = 0; currentPE < NumProcs; currentPE++) {

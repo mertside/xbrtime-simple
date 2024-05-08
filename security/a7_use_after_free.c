c1541686aa12f6cc16b2688ede790bd5430a9e02
Mert Şide <mertside@gmail.com>
2024-05-08 11:30:39 -0500

    Update a7_use_after_free.c

--- security/a7_use_after_free.c
+++ security/a7_use_after_free.c
@@ -13,29 +13,29 @@
 
 // Thread function to demonstrate use-after-free
 void *use_after_free(void *arg) {
-    printf("Starting Test: Use-After-Free\n");
+  printf("Starting Test: Use-After-Free\n");
 
-    char *complete = malloc(sizeof(char) * BUFFER_SIZE);   
-    strcpy(complete, "Hello World!Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod");
+  char *complete = malloc(sizeof(char) * BUFFER_SIZE);   
+  strcpy(complete, "Hello World!Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod");
 
-    printf("Printing characters of string before free:\n");
-    for(int i = 0; i < BUFFER_SIZE; i++) {
-            printf("%c", complete[i]);
-    }
-    printf("%c", '\n');
+  printf("Printing characters of string before free:\n");
+  for(int i = 0; i < BUFFER_SIZE; i++) {
+    printf("%c", complete[i]);
+  }
+  printf("%c", '\n');
 
-    free(complete);
+  free(complete);
 
-    // Dangerous operation: using after free
-    printf("Printing characters of string after free:\n");
-    for(int i = 0; i < BUFFER_SIZE; i++) {
-           printf("%c", complete[i]); 
-    }
+  // Dangerous operation: using after free
+  printf("Printing characters of string after free:\n");
+  for(int i = 0; i < BUFFER_SIZE; i++) {
+    printf("%c", complete[i]); 
+  }
 
-    printf("%c", '\n');
-    printf("Test Failed: Use-After-Free\n\n");
+  printf("%c", '\n');
+  printf("Test Failed: Use-After-Free\n\n");
 
-    return NULL;
+  return NULL;
 }
 
 int main() {
@@ -51,18 +51,5 @@ int main() {
                             NULL);
   }
 
-    // const int num_threads = 4; // Example for 4 PEs
-    // pthread_t threads[num_threads];
-
-    // // Create threads to simulate the use-after-free vulnerability across PEs
-    // for(int i = 0; i < num_threads; i++) {
-    //     pthread_create(&threads[i], NULL, use_after_free, NULL);
-    // }
-
-    // // Wait for all threads to complete
-    // for(int i = 0; i < num_threads; i++) {
-    //     pthread_join(threads[i], NULL);
-    // }
-
-    return 0;
+  return 0;
 }

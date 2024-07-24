d483a3689f874ea221aacb686b8d98f259bbdd7b
Mert Şide <mert.side@ttu.edu>
2024-07-24 13:41:18 -0500

    Update test.sh

--- security/ASU/test.sh
+++ security/ASU/test.sh
@@ -4,13 +4,17 @@ echo "Running ASU tests"
 
 # Save all the output for ASU including segmenation faults and other errors
 
-asuS1=`./asu_s1_free_not_at_start.exe 2>&1`; echo "x$asuS1"
-asuS2=`./asu_s2_free_not_on_heap.exe 2>&1`; echo "x$asuS2"
-asuS3=`./asu_s3_null_ptr_dereference.exe 2>&1`; echo "x$asuS3"
-asuS4=`./asu_s4_oob_read.exe 2>&1`; echo "x$asuS4"
-asuS5=`./asu_s5_oob_write.exe 2>&1`; echo "x$asuS5"
+asuT1=`./asu_t1_double_free.exe 2>&1`; echo "$asuT1" > asu_t1_double_free.txt
+asuT2=`./asu_t2_hm_fake_chunk_malloc.exe 2>&1`; echo "$asuT2" > asu_t2_hm_fake_chunk_malloc.txt
+asuT3=`./asu_t3_hm_house_of_spirit.exe 2>&1`; echo "$asuT3" > asu_t3_hm_house_of_spirit.txt
+asuT4=`./asu_t4_hm_parent_and_child_chunk.exe 2>&1`; echo "$asuT4" > asu_t4_hm_parent_and_child_chunk.txt
+asuT5=`./asu_t5_use_after_free.exe 2>&1`; echo "$asuT5" > asu_t5_use_after_free.txt
 
-
+asuS1=`./asu_s1_free_not_at_start.exe 2>&1`; echo "$asuS1" > asu_s1_free_not_at_start.txt
+asuS2=`./asu_s2_free_not_on_heap.exe 2>&1`; echo "$asuS2" > asu_s2_free_not_on_heap.txt
+asuS3=`./asu_s3_null_ptr_dereference.exe 2>&1`; echo "$asuS3" > asu_s3_null_ptr_dereference.txt
+asuS4=`./asu_s4_oob_read.exe 2>&1`; echo "$asuS4" > asu_s4_oob_read.txt
+asuS5=`./asu_s5_oob_write.exe 2>&1`; echo "$asuS5" > asu_s5_oob_write.txt
 
 # ./asu_s2_free_not_on_heap.exe
 # ./asu_s3_null_ptr_dereference.exe

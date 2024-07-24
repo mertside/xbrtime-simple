95b1cf6fc813d59df97bae79599ee797ee5122d3
Mert Şide <mert.side@ttu.edu>
2024-07-24 13:45:34 -0500

    Update test.sh

--- security/ASU/test.sh
+++ security/ASU/test.sh
@@ -4,17 +4,17 @@ echo "Running ASU tests"
 
 # Save all the output for ASU including segmenation faults and other errors
 
-asuT1=`./asu_t1_double_free.exe 2>&1`; echo "$asuT1" > asu_t1_double_free.txt
-asuT2=`./asu_t2_hm_fake_chunk_malloc.exe 2>&1`; echo "$asuT2" > asu_t2_hm_fake_chunk_malloc.txt
-asuT3=`./asu_t3_hm_house_of_spirit.exe 2>&1`; echo "$asuT3" > asu_t3_hm_house_of_spirit.txt
-asuT4=`./asu_t4_hm_parent_and_child_chunk.exe 2>&1`; echo "$asuT4" > asu_t4_hm_parent_and_child_chunk.txt
-asuT5=`./asu_t5_use_after_free.exe 2>&1`; echo "$asuT5" > asu_t5_use_after_free.txt
+asuT1=`./asu_t1_double_free.exe 2>&1`; echo "ASU_T1:$asuT1" > asu_t1_double_free.txt
+asuT2=`./asu_t2_hm_fake_chunk_malloc.exe 2>&1`; echo "ASU_T2:$asuT2" > asu_t2_hm_fake_chunk_malloc.txt
+asuT3=`./asu_t3_hm_house_of_spirit.exe 2>&1`; echo "ASU_T3:$asuT3" > asu_t3_hm_house_of_spirit.txt
+asuT4=`./asu_t4_hm_parent_and_child_chunk.exe 2>&1`; echo "ASU_T4:$asuT4" > asu_t4_hm_parent_and_child_chunk.txt
+asuT5=`./asu_t5_use_after_free.exe 2>&1`; echo "ASU_T5:$asuT5" > asu_t5_use_after_free.txt
 
-asuS1=`./asu_s1_free_not_at_start.exe 2>&1`; echo "$asuS1" > asu_s1_free_not_at_start.txt
-asuS2=`./asu_s2_free_not_on_heap.exe 2>&1`; echo "$asuS2" > asu_s2_free_not_on_heap.txt
-asuS3=`./asu_s3_null_ptr_dereference.exe 2>&1`; echo "$asuS3" > asu_s3_null_ptr_dereference.txt
-asuS4=`./asu_s4_oob_read.exe 2>&1`; echo "$asuS4" > asu_s4_oob_read.txt
-asuS5=`./asu_s5_oob_write.exe 2>&1`; echo "$asuS5" > asu_s5_oob_write.txt
+# asuS1=`./asu_s1_free_not_at_start.exe 2>&1`; echo "ASU_S1:$asuS1" > asu_s1_free_not_at_start.txt
+# asuS2=`./asu_s2_free_not_on_heap.exe 2>&1`; echo "ASU_S2:$asuS2" > asu_s2_free_not_on_heap.txt
+# asuS3=`./asu_s3_null_ptr_dereference.exe 2>&1`; echo "ASU_S3:$asuS3" > asu_s3_null_ptr_dereference.txt
+# asuS4=`./asu_s4_oob_read.exe 2>&1`; echo "ASU_S4:$asuS4" > asu_s4_oob_read.txt
+# asuS5=`./asu_s5_oob_write.exe 2>&1`; echo "ASU_S5:$asuS5" > asu_s5_oob_write.txt
 
 # ./asu_s2_free_not_on_heap.exe
 # ./asu_s3_null_ptr_dereference.exe

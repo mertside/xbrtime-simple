9768df5a547f221de0958684a52ad080d3fc4386
Mert Şide <mert.side@ttu.edu>
2024-07-24 14:08:09 -0500

    Update test.sh

--- security/ASU/test.sh
+++ security/ASU/test.sh
@@ -2,9 +2,8 @@
 
 echo "Running ASU tests"
 
-# Save all the output for ASU including segmenation faults and other errors
-
-# ./asu_t3_hm_house_of_spirit.exe > asu_t3.out.txt 2> asu_t3.err.txt
+# ----------------------------------------------------------------------------
+echo "==="
 
 echo "ASU_T1:"
 asuT1=`./asu_t1_double_free.exe`;
@@ -26,24 +25,51 @@ echo "ASU_T5:"
 asuT5=`./asu_t5_use_after_free.exe`;
 echo "$asuT5"
 
+# ----------------------------------------------------------------------------
+echo "==="
 
-# asuS1=`./asu_s1_free_not_at_start.exe 2>&1`; echo "ASU_S1:$asuS1" > asu_s1_free_not_at_start.txt
-# asuS2=`./asu_s2_free_not_on_heap.exe 2>&1`; echo "ASU_S2:$asuS2" > asu_s2_free_not_on_heap.txt
-# asuS3=`./asu_s3_null_ptr_dereference.exe 2>&1`; echo "ASU_S3:$asuS3" > asu_s3_null_ptr_dereference.txt
-# asuS4=`./asu_s4_oob_read.exe 2>&1`; echo "ASU_S4:$asuS4" > asu_s4_oob_read.txt
-# asuS5=`./asu_s5_oob_write.exe 2>&1`; echo "ASU_S5:$asuS5" > asu_s5_oob_write.txt
+echo "ASU_S1:"
+asuS1=`./asu_s1_free_not_at_start.exe`;
+echo "$asuS1"
 
-# ./asu_s2_free_not_on_heap.exe
-# ./asu_s3_null_ptr_dereference.exe
-# ./asu_s4_oob_read.exe
-# ./asu_s5_oob_write.exe
-# ./asu_t1_double_free.exe
-# ./asu_t2_hm_fake_chunk_malloc.exe
-# ./asu_t3_hm_house_of_spirit.exe
-# ./asu_t4_hm_parent_and_child_chunk.exe
-# ./asu_t5_use_after_free.exe
-# ./asu_r1_heartbleed.exe
-# ./asu_r2_dop.exe
-# ./asu_r3_uaf_to_code_reuse.exe
-# ./asu_r4_ip_large_size.exe
-# ./asu_r5_df_switch_statements.exe
+echo "ASU_S2:"
+asuS2=`./asu_s2_free_not_on_heap.exe`;
+echo "$asuS2"
+
+echo "ASU_S3:"
+asuS3=`./asu_s3_null_ptr_dereference.exe`;
+echo "$asuS3"
+
+echo "ASU_S4:"
+asuS4=`./asu_s4_oob_read.exe`;
+echo "$asuS4"
+
+echo "ASU_S5:"
+asuS5=`./asu_s5_oob_write.exe`;
+echo "$asuS5"
+
+# ----------------------------------------------------------------------------
+echo "==="
+
+echo "ASU_R1:"
+asuR1=`./asu_r1_heartbleed.exe`;
+echo "$asuR1"
+
+echo "ASU_R2:"
+asuR2=`./asu_r2_dop.exe`;
+echo "$asuR2"
+
+echo "ASU_R3:"
+asuR3=`./asu_r3_uaf_to_code_reuse.exe`;
+echo "$asuR3"
+
+echo "ASU_R4:"
+asuR4=`./asu_r4_ip_large_size.exe`;
+echo "$asuR4"
+
+echo "ASU_R5:"
+asuR5=`./asu_r5_df_switch_statements.exe`;
+echo "$asuR5"
+
+# ----------------------------------------------------------------------------
+echo "==="

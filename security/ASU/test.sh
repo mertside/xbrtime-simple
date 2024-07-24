3a0abe411c2da3dcf084aaf20fcfbc4fc95187fb
Mert Şide <mert.side@ttu.edu>
2024-07-24 13:58:07 -0500

    Update test.sh

--- security/ASU/test.sh
+++ security/ASU/test.sh
@@ -4,11 +4,28 @@ echo "Running ASU tests"
 
 # Save all the output for ASU including segmenation faults and other errors
 
-asuT1=`./asu_t1_double_free.exe 2>&1`; echo "ASU_T1:$asuT1" > asu_t1_double_free.txt
-asuT2=`./asu_t2_hm_fake_chunk_malloc.exe 2>&1`; echo "ASU_T2:$asuT2" > asu_t2_hm_fake_chunk_malloc.txt
-asuT3=`./asu_t3_hm_house_of_spirit.exe 2>&1`; echo "ASU_T3:$asuT3" > asu_t3_hm_house_of_spirit.txt
-asuT4=`./asu_t4_hm_parent_and_child_chunk.exe 2>&1`; echo "ASU_T4:$asuT4" > asu_t4_hm_parent_and_child_chunk.txt
-asuT5=`./asu_t5_use_after_free.exe 2>&1`; echo "ASU_T5:$asuT5" > asu_t5_use_after_free.txt
+# ./asu_t3_hm_house_of_spirit.exe > asu_t3.out.txt 2> asu_t3.err.txt
+
+echo "ASU_T1:"
+asuT1=`./asu_t1_double_free.exe 2>&1`;
+echo "$asuT1"
+
+echo "ASU_T2:"
+asuT2=`./asu_t2_hm_fake_chunk_malloc.exe 2>&1`;
+echo "$asuT2"
+
+echo "ASU_T3:"
+asuT3=`./asu_t3_hm_house_of_spirit.exe 2>&1`;
+echo "$asuT3"
+
+echo "ASU_T4:"
+asuT4=`./asu_t4_hm_parent_and_child_chunk.exe 2>&1`;
+echo "$asuT4"
+
+echo "ASU_T5:"
+asuT5=`./asu_t5_use_after_free.exe 2>&1`;
+echo "$asuT5"
+
 
 # asuS1=`./asu_s1_free_not_at_start.exe 2>&1`; echo "ASU_S1:$asuS1" > asu_s1_free_not_at_start.txt
 # asuS2=`./asu_s2_free_not_on_heap.exe 2>&1`; echo "ASU_S2:$asuS2" > asu_s2_free_not_on_heap.txt

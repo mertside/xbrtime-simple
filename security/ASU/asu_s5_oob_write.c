72710a730b467ce7ee7223451060f10b91c85c28
Mert Şide <mert.side@ttu.edu>
2024-08-13 14:43:16 -0500

    Update asu_s5_oob_write.c

--- security/ASU/asu_s5_oob_write.c
+++ security/ASU/asu_s5_oob_write.c
@@ -1,7 +1,7 @@
 /*  Benchmark: Out-of-Bounds Write
  *  @author  : Secure, Trusted, and Assured Microelectronics (STAM) Center
 
- *  Copyright (c) 2023 Trireme (STAM/SCAI/ASU)
+ *  Copyright (c) 2024 Memory Safety Benchmarks (STAM/SCAI/ASU)
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights

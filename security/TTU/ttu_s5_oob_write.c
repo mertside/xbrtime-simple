ca7b4ceb2b45b51230266eef4ccee0a16725d5d8
Mert Şide <mert.side@ttu.edu>
2024-08-16 14:10:52 -0500

    Update ttu_s5_oob_write.c

--- security/TTU/ttu_s5_oob_write.c
+++ security/TTU/ttu_s5_oob_write.c
@@ -91,7 +91,7 @@ int main() {
 
   printf("Starting test: Out of Bounds Write\n");
 
-  for (int i = 0; i < num_pes; i++) {
+  for (long i = 0; i < num_pes; i++) {
     bool check = false;
     check = tpool_add_work(threads[i].thread_queue,
                             out_of_bounds_write,

1a6a05ec43316d9d943f0dc80eaeb06ca7ef72a4
Mert Şide <mert.side@ttu.edu>
2024-08-26 14:08:32 -0500

    Update test.sh

--- security/TTU/test.sh
+++ security/TTU/test.sh
@@ -25,6 +25,40 @@ echo "============================================================"
 echo " "
 
 echo "------------------------------"
+echo "TTU_S1: Free Not At Start"
+ttuS1=`./ttu_s1_free_not_at_start.exe`;
+echo "$ttuS1"
+echo " "
+
+echo "------------------------------"
+echo "TTU_S2: Free Not On Heap"
+ttuS2=`./ttu_s2_free_not_on_heap.exe`;
+echo "$ttuS2"
+echo " "
+
+echo "------------------------------"
+echo "TTU_S3: Null Pointer Dereference"
+ttuS3=`./ttu_s3_null_ptr_dereference.exe`;
+echo "$ttuS3"
+echo " "
+
+echo "------------------------------"
+echo "TTU_S4: Out Of Bounds Read"
+ttuS4=`./ttu_s4_oob_read.exe`;
+echo "$ttuS4"
+echo " "
+
+echo "------------------------------"
+echo "TTU_S5: Out Of Bounds Write"
+ttuS5=`./ttu_s5_oob_write.exe`;
+echo "$ttuS5"
+echo " "
+
+# ----------------------------------------------------------------------------
+echo "============================================================"
+echo " "
+
+echo "------------------------------"
 echo "TTU_T1: Double Free"
 ttuT1=`./ttu_t1_double_free.exe`;
 echo "$ttuT1"
@@ -55,39 +89,16 @@ ttuT5=`./ttu_t5_use_after_free.exe`;
 echo "$ttuT5"
 echo " "
 
-# ----------------------------------------------------------------------------
-echo "============================================================"
+echo "------------------------------"
+echo "TTU_T6: UAF Function Pointer"
+ttuT6=`./ttu_t6_uaf_function_pointer.exe`;
+echo "$ttuT6"
 echo " "
 
 echo "------------------------------"
-echo "TTU_S1: Free Not At Start"
-ttuS1=`./ttu_s1_free_not_at_start.exe`;
-echo "$ttuS1"
-echo " "
-
-echo "------------------------------"
-echo "TTU_S2: Free Not On Heap"
-ttuS2=`./ttu_s2_free_not_on_heap.exe`;
-echo "$ttuS2"
-echo " "
-
-echo "------------------------------"
-echo "TTU_S3: Null Pointer Dereference"
-ttuS3=`./ttu_s3_null_ptr_dereference.exe`;
-echo "$ttuS3"
-echo " "
-
-echo "------------------------------"
-echo "TTU_S4: Out Of Bounds Read"
-ttuS4=`./ttu_s4_oob_read.exe`;
-echo "$ttuS4"
-echo " "
-
-echo "------------------------------"
-echo "TTU_S5: Out Of Bounds Write"
-ttuS5=`./ttu_s5_oob_write.exe`;
-echo "$ttuS5"
-echo " "
+echo "TTU_T7: UAF Memcpy"
+ttuT7=`./ttu_t7_uaf_memcpy.exe`;
+echo "$ttuT7"
 
 # ----------------------------------------------------------------------------
 echo "============================================================"
